3e35602b90ba89f75fec4fce7feb36b63656cf08
jacksonchenzhihao <jacksonchenzhihao@didiglobal.com>
2021-11-16 16:42:04 +0800

    upd

--- hi-school-bootstrap/src/main/java/cc/chenzhihao/hischool/bootstrap/HiSchoolAppApplication.java
+++ hi-school-bootstrap/src/main/java/cc/chenzhihao/hischool/bootstrap/HiSchoolAppApplication.java
@@ -1,5 +1,6 @@
 package cc.chenzhihao.hischool.bootstrap;
 
+import cc.chenzhihao.hi.framework.spring.enable.EnableSpringBeanContext;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "cc.chenzhihao.hischool")
 @MapperScan(basePackages = "cc.chenzhihao.hischool.infrastructure.persistent.mapper")
+@EnableSpringBeanContext
 public class HiSchoolAppApplication {
 
     public static void main(String[] args) {
